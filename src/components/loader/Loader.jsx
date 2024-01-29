c99cdba7671404e6d37b63a7341053bb0966771f
PiyushG-dev <pjgpiyush@gmail.com>
2024-01-29 23:14:00 +0530

    loader component updated

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,46 +1,32 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./Loader.module.css";
 import { gsap } from "gsap";
+import SplitType from "split-type";
 
 const Loader = () => {
-  const con = useRef(null);
-  const one = useRef(null);
-  const two = useRef(null);
-  const three = useRef(null);
+  const wrapper = useRef(null);
+  const tagline = useRef(null);
 
   useEffect(() => {
-    gsap.fromTo(
-      one.current,
-      { x: 40, opacity: 0 },
-      { x: 0, opacity: 1, duration: 1.5, delay: 1 }
+    const myText = new SplitType(tagline.current);
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      myText.words,
+      { x: 20, opacity: 0 },
+      { x: 0, opacity: 1, duration: 1, stagger: 0.1 }
     );
 
-    gsap.fromTo(
-      two.current,
-      { x: 40, opacity: 0 },
-      { x: 0, opacity: 1, duration: 1.5, delay: 1.1 }
-    );
+    tl.to(myText.words, { x: -20, opacity: 0, stagger: 0.1 });
 
-    gsap.fromTo(
-      three.current,
-      { x: 40, opacity: 0 },
-      { x: 0, opacity: 1, duration: 1.5, delay: 1.2 }
-    );
-
-    gsap.to(one.current, { x: -40, opacity: 0, delay: 2 });
-    gsap.to(two.current, { x: -40, opacity: 0, delay: 2.1 });
-    gsap.to(three.current, { x: -40, opacity: 0, delay: 2.2 });
-
-    gsap.to(con.current, { duration: 1, y: -600, zIndex: -10, delay: 2.8 });
-    gsap.to(con.current, { duration: 3, y: -1000, zIndex: -10, delay: 3.6 });
+    tl.to(wrapper.current, { opacity: 0 });
+    tl.to(wrapper.current, { display: "none" });
   }, []);
 
   return (
-    <div ref={con} className={styles.container}>
-      <h3 ref={one}>Tommorow's</h3>
-      <h3 ref={two}>Brand,</h3>
-      <h3 ref={three}>Today.&#8482;</h3>
-    </div>
+    <section ref={wrapper} className={styles.wrapper}>
+      <h3 ref={tagline}>Tommorow's Brand Today &#8482;</h3>
+    </section>
   );
 };
 
